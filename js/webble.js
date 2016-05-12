ec896ef74cf34e2024f2e0d6c9cca9c3f9d6441e
jeffyactive <jeff@reelyactive.com>
2016-05-12 16:05:39 -0400

    Added error handling

--- js/webble.js
+++ js/webble.js
@@ -1,14 +1,25 @@
 angular.module('webble', [ 'ui.bootstrap' ])
 
   // Interaction controller
-  .controller('InteractionCtrl', function($scope) {
+  .controller('InteractionCtrl', function($scope, $interval) {
     $scope.device = JSON.stringify({}, null, ' ');
+    $scope.scanCount = 0;
 
-    navigator.bluetooth.requestDevice({
-      filters: [{
-        name: 'reelyActive'
-      }]
-    })
-    .then(device => { $scope.device = JSON.stringify(device, null, ' '); })
-    .catch(error => { $scope.device = { error: error }; });
+    function scan() {
+      $scope.scanCount++;
+      try {
+        navigator.bluetooth.requestDevice({
+          filters: [{
+            name: 'reelyActive'
+          }]
+        })
+        .then(device => { $scope.device = JSON.stringify(device, null, ' '); })
+        .catch(error => { $scope.device = { error: error }; });
+      }
+      catch(err) {
+        $scope.device = { err: err.toString() };
+      }
+    }
+
+    $interval(scan, 4000);
   });
