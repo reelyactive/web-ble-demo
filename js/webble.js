fe85845231257bf653f003cffb2189f4867b6ba5
jeffyactive <jeff@reelyactive.com>
2016-05-14 12:42:48 -0400

    Major revamp

--- js/webble.js
+++ js/webble.js
@@ -2,23 +2,21 @@ angular.module('webble', [ 'ui.bootstrap' ])
 
   // Interaction controller
   .controller('InteractionCtrl', function($scope, $interval) {
-    $scope.result = {};
-    $scope.compatibilityError = 'None';
-    $scope.scanError = 'None';
+    $scope.result = null;
+    $scope.nameFilter = 'reelyActive';
     $scope.isChrome = !!window.chrome && !!window.chrome.webstore;
+    $scope.compatibilityError = null;
+    $scope.scanError = null;
 
     $scope.scan = function() {
       try {
         navigator.bluetooth.requestDevice({
           filters: [{
-            name: 'reelyActive',
-            optionalServices: [0x2a23, 0x7265656c794163746976652055554944]
+            name: nameFilter
           }]
         })
         .then(device => {
-          $scope.result.name = device.name;
-          $scope.result.uuids = device.uuids;
-          $scope.result.device = device;
+          $scope.result = { name: device.name, uuids: device.uuids };
           return device.gatt.connect();
         })
         .catch(error => { $scope.scanError = error.toString(); });
