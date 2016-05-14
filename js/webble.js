c08420743017c5e0a9404687e8e4885e76dfaf39
jeffyactive <jeff@reelyactive.com>
2016-05-14 07:11:21 -0400

    Oops

--- js/webble.js
+++ js/webble.js
@@ -13,8 +13,8 @@ angular.module('webble', [ 'ui.bootstrap' ])
           filters: [{
             //name: 'reelyActive'
             //services: ['battery_service', 'heart_rate', 0xfed8, 0xfeed, 0xcbbfe0e1f7f3420684e084cbb3d09dfc]
-          }]
             services: [0xcbbfe0e1f7f3420684e084cbb3d09dfc]
+          }]
         })
         .then(device => {
           $scope.device.name = device.name;
