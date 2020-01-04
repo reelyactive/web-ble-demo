6a120c4e6193857f7e93e5fe61b4b1497f7bffc4
jeffyactive <jeff@reelyactive.com>
2020-01-03 22:09:11 -0500

    Updated scan results

--- js/webble.js
+++ js/webble.js
@@ -16,8 +16,16 @@ let eventJson = document.querySelector('#eventJson');
 let scanStatus = document.querySelector('#scanStatus');
 
 
+// Other variables
+let devices = {};
+
+
 // Attempt to run the experimental requestLEScan function
 async function scanForAdvertisements() {
+  devices = {};
+  eventJson.textContent = '';
+  scanStatus.textContent = 'Initiating scan';
+
   try {
     const scan = await navigator.bluetooth.requestLEScan(SCAN_OPTIONS);
     let numberOfEvents = 0;
@@ -25,12 +33,14 @@ async function scanForAdvertisements() {
     scanStatus.textContent = 'Scan started: active = ' + scan.active;
 
     navigator.bluetooth.addEventListener('advertisementreceived', event => {
+      devices[event.device.id] = { rssi: event.rssi };
       eventJson.textContent = JSON.stringify(event, null, 2);
       numberOfEvents++;
     });
 
     function stopScan() {
       scan.stop();
+      eventJson.textContent = JSON.stringify(devices, null, 2);
       scanStatus.textContent = 'Scan stopped.  ' + numberOfEvents +
                                ' events detected.';
       stopButton.removeEventListener('click', stopScan);
